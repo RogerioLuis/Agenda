359fc209aa16b6dda9a9b943478ba9188ea8b2ce
Rodrigo <144171797+Rodrigo5431@users.noreply.github.com>
2023-09-20 21:26:23 -0300

    Delete funciona infeliz

--- AgendaTelefonicaJS/app.js
+++ AgendaTelefonicaJS/app.js
@@ -1,9 +1,9 @@
 function agenda() {
    let contatoNovo;
-   let lista = [ 
-      ["Rodrigo Coutinho", "(24) 99999-9999","3000798", "aaaaa@sdsfsdaf"],
+   let lista = [
+      ["Rodrigo Coutinho", "(24) 99999-9999", "3000798", "aaaaa@sdsfsdaf"],
       ["Rodrigo Lima", "(24) 88888 8888", "484664", "dfssfd@jhglglhk"],
-      ["Lucas Lima","(21) 77777 7171", "466556", "gsdfghf@jhk"]
+      ["Lucas Lima", "(21) 77777 7171", "466556", "gsdfghf@jhk"]
    ];
    let select;
    let nome;
@@ -11,54 +11,60 @@ function agenda() {
    let numero;
    let email;
    let contador = 1;
+   let id;
    let ativo = true;
-   let excluir;
-   
+
    function adicionarContato() {
-      
+
+      id = contador++;
       nome = prompt("Digite o nome do contato");
       dataNasc = prompt("Digite a data de nascimento");
       numero = prompt("Telefone do contato");
       email = prompt("Digite o e-mail do contato");
-      contatoNovo = [nome, numero, dataNasc, email, true];
+      contatoNovo = [id, nome, numero, dataNasc, email];
       lista.push(contatoNovo);
       alert("Contato Salvo");
    }
+
    function listarContatos() {
-      resposta = "";
-      for(let i=0; i<lista.length; i++){
-         contato = lista[i];
-         resposta = resposta + (`${i+1}- ${contato[0]}`) + "\n";
-         let opcao = prompt(resposta);
-      }
+      alert(lista.join("\n"));
    }
-   function excluirContato (){
-      lista.splice (1,1);
+
+   function excluirContato() {
+      let escolha = prompt("Contato que deseja excluir" + listarContatos());
+      lista.splice(escolha);
+      
    }
    
-
+   
    
    while (select != 4) {
       select = parseInt(prompt("Agenda telefônica!! \n \n1. Adicionar Novo Contato \n2. Exibir Lista \n3. Excluir Contato "));
-     
+      
       switch (select) {
          case 1:
             adicionarContato();
             break;
-         case 2:
-            listarContatos();
-            break;
-
-            case 3: 
-            excluirContato();
-            
-           
-         default:
-            alert("Opção inválida, escolha uma opção válida");
+            case 2:
+               listarContatos();
+               break;
+               
+               case 3:
+                  excluirContato();
+                  break;
+                  
+                  
+                  default:
+                     alert("Opção inválida, escolha uma opção válida");
+                     agenda();
+                  }
+               }
+               
+               
+            }
+            // resposta = "";
+            // for(let i=0; i < lista.length; i++){
+            //    contato = lista[i];
+            //    resposta = resposta + (`${i+1}- ${contato[0]}`) + "\n";
             agenda();
-      }
-   }
-
-
-}
-agenda();
+            
